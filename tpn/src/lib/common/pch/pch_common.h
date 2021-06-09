f00102f4622e1082dbe3df8a115a02c3e1bc0326
stanleyguo0207 <stanleyguo0207@163.com>
2021-06-10 00:13:00 +0800

    feat: 添加库CMakeLists.txt模板 添加common库
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

---
+++ tpn/src/lib/common/pch/pch_common.h
@@ -0,0 +1,76 @@
+//
+//           ┌┬┐┬ ┬┌─┐┬ ┬┌─┐┌─┐┌┐┌
+//            │ └┬┘├─┘├─┤│ ││ ││││
+//            ┴  ┴ ┴  ┴ ┴└─┘└─┘┘└┘
+//
+// This file is part of the typhoon Project.
+// Copyright (C) 2021 stanley0207@163.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef TYPHOON_ZERO_TPN_SRC_LIB_COMMON_PCH_PCH_COMMON_H_
+#define TYPHOON_ZERO_TPN_SRC_LIB_COMMON_PCH_PCH_COMMON_H_
+
+// rapidjson
+#include <rapidjson/rapidjson.h>
+#include <rapidjson/document.h>
+#include <rapidjson/filereadstream.h>
+
+// fmt
+#include <fmt/chrono.h>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <fmt/printf.h>
+
+// sfmt
+#include <sfmt/sfmt.h>
+
+// utfcpp
+#include <utfcpp/utf8.h>
+
+#include <set>
+#include <list>
+#include <mutex>
+#include <queue>
+#include <array>
+#include <vector>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <limits>
+#include <random>
+#include <memory>
+#include <atomic>
+#include <utility>
+#include <optional>
+#include <algorithm>
+#include <functional>
+#include <filesystem>
+#include <string_view>
+#include <type_traits>
+#include <unordered_set>
+#include <unordered_map>
+#include <condition_variable>
+
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <cstdarg>
+
+#endif  // TYPHOON_ZERO_TPN_SRC_LIB_COMMON_PCH_PCH_COMMON_H_
