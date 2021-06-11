f4ad120ca04f07a32ca4e5d834eb99b901956043
stanleyguo0207 <stanleyguo0207@163.com>
2021-06-11 19:01:53 +0800

    feat: 配置管理器更新
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- tpn/src/lib/common/config/config.cpp
+++ tpn/src/lib/common/config/config.cpp
@@ -12,7 +12,7 @@
 // (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// but WITHOUT ANY WARRANTY{} without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
@@ -22,7 +22,162 @@
 
 #include "config.h"
 
+#include <filesystem>
+
+#include <rapidjson/filereadstream.h>
+
+#include "debug_hub.h"
+
+namespace fs = std::filesystem;
+
 namespace tpn {
+
+bool ConfigMgr::Load(std::string_view path, std::vector<std::string> args,
+                     std::string &error, bool reload /*  = false */) {
+  std::lock_guard<std::mutex> lock(load_mutex_);
+
+  if (!reload) {
+    path_ = path;
+    args_ = std::move(args);
+  }
+
+  auto json_path = fs::path(path);
+  json_path.make_preferred();
+  auto json_file = fs::absolute(json_path);
+  try {
+#if (TPN_COMPILER == TPN_COMPILER_MSVC)
+    const char *mask = "rb";
+#else
+    const char *mask = "r";
+#endif
+    auto *fp  = std::fopen(json_file.generic_string().c_str(), mask);
+    auto len  = fs::file_size(json_path);
+    char *buf = new char[len + 1];
+    rapidjson::FileReadStream is(fp, buf, len + 1);
+    document_.SetNull();
+    document_.ParseStream<rapidjson::kParseCommentsFlag>(is);
+    std::fclose(fp);
+    delete[] buf;
+
+    if (document_.HasParseError()) {
+      error = std::string("parse error") + " (" + path_ + ") ";
+      return false;
+    }
+  } catch (fs::filesystem_error &e) {
+    error = std::string{e.what()} + " (" + path_ + ") ";
+    return false;
+  } catch (const std::exception &ex) {
+    error = std::string{ex.what()} + " (" + path_ + ") ";
+    return false;
+  }
+
+  return true;
+}
+
+bool ConfigMgr::Reload(std::string &error) { return Load({}, {}, error, true); }
+
+std::string_view ConfigMgr::GetPath() const { return path_; }
+
+const std::vector<std::string> &ConfigMgr::GetArguments() const {
+  return args_;
+}
+
+std::string ConfigMgr::GetStringDefault(std::string_view key,
+                                        std::string_view def) const {
+  TPN_ASSERT(!document_.IsNull(), "Please Call Load Before");
+  if (document_.HasMember(key.data())) {
+    TPN_ASSERT(document_[key.data()].IsString(),
+               "Wrong Type not string key:{} def:{} key_type:{}", key, def,
+               document_[key.data()].GetType());
+    return std::string{document_[key.data()].GetString()};
+  } else {
+    return std::string{def};
+  }
+}
+
+bool ConfigMgr::GetBoolDefault(std::string_view key, bool def) const {
+  TPN_ASSERT(!document_.IsNull(), "Please Call Load Before");
+  if (document_.HasMember(key.data())) {
+    TPN_ASSERT(document_[key.data()].IsBool(),
+               "Wrong Type not bool key:{} def:{} key_type:{}", key, def,
+               document_[key.data()].GetType());
+    return document_[key.data()].GetBool();
+  } else {
+    return def;
+  }
+}
+
+int32_t ConfigMgr::GetI32Default(std::string_view key, int32_t def) const {
+  TPN_ASSERT(!document_.IsNull(), "Please Call Load Before");
+  if (document_.HasMember(key.data())) {
+    TPN_ASSERT(document_[key.data()].IsInt(),
+               "Wrong Type not int key:{} def:{} key_type:{}", key, def,
+               document_[key.data()].GetType());
+    return document_[key.data()].GetInt();
+  } else {
+    return def;
+  }
+}
+
+int64_t ConfigMgr::GetI64Default(std::string_view key, int64_t def) const {
+  TPN_ASSERT(!document_.IsNull(), "Please Call Load Before");
+  if (document_.HasMember(key.data())) {
+    TPN_ASSERT(document_[key.data()].IsInt64(),
+               "Wrong Type not int64 key:{} def:{} key_type:{}", key, def,
+               document_[key.data()].GetType());
+    return document_[key.data()].GetInt64();
+  } else {
+    return def;
+  }
+}
+
+uint32_t ConfigMgr::GetU32Default(std::string_view key, uint32_t def) const {
+  TPN_ASSERT(!document_.IsNull(), "Please Call Load Before");
+  if (document_.HasMember(key.data())) {
+    TPN_ASSERT(document_[key.data()].IsUint(),
+               "Wrong Type not uint key:{} def:{} key_type:{}", key, def,
+               document_[key.data()].GetType());
+    return document_[key.data()].GetUint();
+  } else {
+    return def;
+  }
+}
+
+uint64_t ConfigMgr::GetU64Default(std::string_view key, uint64_t def) const {
+  TPN_ASSERT(!document_.IsNull(), "Please Call Load Before");
+  if (document_.HasMember(key.data())) {
+    TPN_ASSERT(document_[key.data()].IsUint64(),
+               "Wrong Type not uint64 key:{} def:{} key_type:{}", key, def,
+               document_[key.data()].GetType());
+    return document_[key.data()].GetUint64();
+  } else {
+    return def;
+  }
+}
+
+float ConfigMgr::GetFloatDefault(std::string_view key, float def) const {
+  TPN_ASSERT(!document_.IsNull(), "Please Call Load Before");
+  if (document_.HasMember(key.data())) {
+    TPN_ASSERT(document_[key.data()].IsFloat(),
+               "Wrong Type not float key:{} def:{} key_type:{}", key, def,
+               document_[key.data()].GetType());
+    return document_[key.data()].GetFloat();
+  } else {
+    return def;
+  }
+}
+
+double ConfigMgr::GetDoubleDefault(std::string_view key, double def) const {
+  TPN_ASSERT(!document_.IsNull(), "Please Call Load Before");
+  if (document_.HasMember(key.data())) {
+    TPN_ASSERT(document_[key.data()].IsDouble(),
+               "Wrong Type not double key:{} def:{} key_type:{}", key, def,
+               document_[key.data()].GetType());
+    return document_[key.data()].GetDouble();
+  } else {
+    return def;
+  }
+}
 
 TPN_SINGLETON_IMPL(ConfigMgr)
 }  // namespace tpn
--- tpn/tests/lib/common/test_common.cpp
+++ tpn/tests/lib/common/test_common.cpp
@@ -34,3 +34,43 @@ TEST_CASE("debug_hub", "[common]") {
   TPN_ASSERT(3 < 5, "assert 3 < 5");
   TPN_ASSERT(3 > 5, "assert 3 > 5 maybe error : time{}", time(nullptr));
 }
+
+// configmgr
+#include "config.h"
+
+TEST_CASE("config", "[common]") {
+#ifndef _TPN_COMMON_CONFIG_TEST_FILE
+#  define _TPN_COMMON_CONFIG_TEST_FILE "config_common_test.json"
+#endif
+
+  std::string error;
+  g_config->Load(_TPN_COMMON_CONFIG_TEST_FILE, {}, error);
+  if (!error.empty()) {
+    cout << error.c_str() << endl;
+    return;
+  }
+
+  auto host = g_config->GetStringDefault("host", "127.0.0.1");
+  cout << "host: " << host.c_str() << endl;
+
+  auto num_1 = g_config->GetI32Default("num_1", -1);
+  cout << "num_1: " << num_1 << endl;
+
+  auto num_2 = g_config->GetI64Default("num_2", 1);
+  cout << "num_2: " << num_2 << endl;
+
+  auto num_3 = g_config->GetU32Default("num_3", 2);
+  cout << "num_3: " << num_3 << endl;
+
+  auto num_4 = g_config->GetU64Default("num_4", 3);
+  cout << "num_4: " << num_4 << endl;
+
+  auto num_5 = g_config->GetU64Default("num_5", 4);
+  cout << "num_5: " << num_5 << endl;
+
+  auto float_1 = g_config->GetFloatDefault("float_1", 1.0);
+  cout << "float_1: " << float_1 << endl;
+
+  auto double_1 = g_config->GetDoubleDefault("double_1", -1.0);
+  cout << "double_1: " << double_1 << endl;
+}
