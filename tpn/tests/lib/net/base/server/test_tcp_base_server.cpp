3abbe6bcbab2538fe8c917ba4a72bcbc00eb6cb7
stanleyguo0207 <stanleyguo0207@163.com>
2021-07-03 15:38:00 +0800

    feat: 网络模块更新
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- tpn/tests/lib/net/base/server/test_tcp_base_server.cpp
+++ tpn/tests/lib/net/base/server/test_tcp_base_server.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
   std::string_view host = "0.0.0.0";
   std::string_view port = "9990";
 
-  net::TcpServer server;
+  net::TcpServer server(4, 200, 100);
 
   LOG_INFO("Tcp base server start init...");
 
