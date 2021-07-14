cd6d84df3e42fbf56c8f6393dca7691f74e0df40
stanleyguo0207 <stanleyguo0207@163.com>
2021-07-15 00:09:01 +0800

    feat: 减少gpt的代码，只对cpp有效
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- tpn/third_party/protobuf/protobuf/libprotoc.cmake
+++ tpn/third_party/protobuf/protobuf/libprotoc.cmake
@@ -16,70 +16,8 @@ set(PROTOC_SOURCES_FILES
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/cpp/cpp_primitive_field.cc
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/cpp/cpp_service.cc
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/cpp/cpp_string_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_doc_comment.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_enum.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_enum_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_field_base.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_generator.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_helpers.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_map_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_message.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_message_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_primitive_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_reflection_class.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_repeated_enum_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_repeated_message_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_repeated_primitive_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_source_generator_base.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_wrapper_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_context.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_doc_comment.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_enum.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_enum_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_enum_field_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_enum_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_extension.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_extension_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_file.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_generator.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_generator_factory.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_helpers.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_kotlin_generator.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_map_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_map_field_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_builder.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_builder_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_field_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_name_resolver.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_primitive_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_primitive_field_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_service.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_shared_code_generator.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_string_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_string_field_lite.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/js/js_generator.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/js/well_known_types_embed.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_enum.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_enum_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_extension.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_file.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_generator.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_helpers.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_map_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_message.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_message_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_oneof.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_primitive_field.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/php/php_generator.cc
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/plugin.cc
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/plugin.pb.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/python/python_generator.cc
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/ruby/ruby_generator.cc
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/subprocess.cc
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/zip_writer.cc
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/pgt_custom/pgt_custom_options.pb.cc
@@ -104,62 +42,6 @@ set(PROTOC_SOURCES_INCLUDES
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/cpp/cpp_primitive_field.h
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/cpp/cpp_service.h
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/cpp/cpp_string_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_doc_comment.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_enum.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_enum_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_field_base.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_helpers.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_map_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_message.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_message_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_options.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_primitive_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_reflection_class.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_repeated_enum_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_repeated_message_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_repeated_primitive_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_source_generator_base.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/csharp/csharp_wrapper_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_context.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_doc_comment.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_enum.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_enum_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_enum_field_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_enum_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_extension.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_extension_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_file.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_generator_factory.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_helpers.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_map_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_map_field_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_builder.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_builder_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_field_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_message_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_name_resolver.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_options.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_primitive_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_primitive_field_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_service.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_shared_code_generator.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_string_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/java/java_string_field_lite.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_enum.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_enum_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_extension.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_file.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_helpers.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_map_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_message.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_message_field.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_nsobject_methods.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_oneof.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/objectivec/objectivec_primitive_field.h
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/scc.h
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/subprocess.h
   ${CMAKE_CURRENT_SOURCE_DIR}/protobuf/google/protobuf/compiler/zip_writer.h
