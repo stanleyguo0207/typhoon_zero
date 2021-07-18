ce5517ee5fcd772014dc208671e83ec2104e5b86
stanleyguo0207 <stanleyguo0207@163.com>
2021-07-19 00:38:56 +0800

    feat: protobuf map测试 json测试
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- tpn/tests/third_party/protobuf/test_protobuf.cpp
+++ tpn/tests/third_party/protobuf/test_protobuf.cpp
@@ -141,7 +141,7 @@ void ListPeopleJson(const tutorial::AddressBook &address_book) {
   std::string json_string;
 
   JsonPrintOptions options;
-  options.add_whitespace = true;
+  options.add_whitespace                = true;
   options.always_print_primitive_fields = true;
   options.preserve_proto_field_names    = true;
 
@@ -169,4 +169,95 @@ TEST_CASE("addressbook read", "[protobuf]") {
   ListPeople(address_book);
 
   ListPeopleJson(address_book);
+}
+
+#include "complex_type.pb.h"
+
+TEST_CASE("complex", "[protobuf]") {
+  using namespace google::protobuf::util;
+
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  std::shared_ptr<void> protobuf_handle(
+      nullptr, [](void *) { google::protobuf::ShutdownProtobufLibrary(); });
+
+  test::ComplexObject obj;
+  obj.set_id(12345);
+  obj.set_name("complex obj");
+  obj.add_phones("1122334455");
+  obj.add_phones("1122334466");
+  obj.set_gender(test::Gender::kGenderMan);
+  auto ret1 = obj.add_results();
+  ret1->set_url("ret1");
+  ret1->set_title("ret1");
+  ret1->add_nums(20);
+  ret1->add_nums(21);
+  auto ret2 = obj.add_results();
+  ret2->set_url("ret2");
+  ret2->set_title("ret2");
+  ret2->add_nums(22);
+  ret2->add_nums(23);
+  test::MapInfo info1;
+  info1.set_id(1);
+  info1.set_name("info1");
+  obj.mutable_map_info()->insert({info1.id(), std::move(info1)});
+  test::MapInfo info2;
+  info2.set_id(2);
+  info2.set_name("info2");
+  obj.mutable_map_info()->insert({info2.id(), std::move(info2)});
+
+  std::string json_string;
+
+  JsonPrintOptions options;
+  options.add_whitespace                = true;
+  options.always_print_primitive_fields = true;
+  options.preserve_proto_field_names    = true;
+
+  MessageToJsonString(obj, &json_string, options);
+
+  fmt::print("JSON: {}", json_string);
+
+  auto iter = obj.map_info().find(1);
+  if (obj.map_info().end() != iter) {
+    fmt::print("map info key:{} name:{}\n", iter->second.id(),
+               iter->second.name());
+  }
+
+  {
+    fstream output("complex_obj", ios::out | ios::trunc | ios::binary);
+    if (!obj.SerializeToOstream(&output)) {
+      fmt::print(stderr, "Failed to write complex obj.\n");
+      return;
+    }
+  }
+}
+
+TEST_CASE("complex read", "[protobuf]") {
+  using namespace google::protobuf::util;
+
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  std::shared_ptr<void> protobuf_handle(
+      nullptr, [](void *) { google::protobuf::ShutdownProtobufLibrary(); });
+
+  test::ComplexObject obj;
+
+  {
+    fstream input("complex_obj", ios::in | ios::binary);
+    if (!obj.ParseFromIstream(&input)) {
+      fmt::print(stderr, "Failed to parse complex obj.\n");
+      return;
+    }
+  }
+
+  std::string json_string;
+
+  JsonPrintOptions options;
+  options.add_whitespace                = true;
+  options.always_print_primitive_fields = true;
+  options.preserve_proto_field_names    = true;
+
+  MessageToJsonString(obj, &json_string, options);
+
+  fmt::print("JSON: {}", json_string);
 }
