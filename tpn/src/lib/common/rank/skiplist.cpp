a92b2c0a875859c7ba17683df17035eb0c4cc034
stanleyguo0207 <stanleyguo0207@163.com>
2021-07-31 17:26:01 +0800

    feat: 更新跳表
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- tpn/src/lib/common/rank/skiplist.cpp
+++ tpn/src/lib/common/rank/skiplist.cpp
@@ -22,4 +22,80 @@
 
 #include "skiplist.h"
 
-namespace tpn {}  // namespace tpn
+#include "random_hub.h"
+
+namespace tpn {
+
+SkipListLevel::SkipListLevel() {}
+
+SkipListLevel::~SkipListLevel() {}
+
+SkipListNode::SkipListNode(int level) {
+  levels_ = std::make_unique<SkipListLevel[]>(level);
+}
+
+SkipListNode::SkipListNode(int level, SkipListNodeUakArrUptr uaks)
+    : uaks_(std::move(uaks)) {
+  levels_ = std::make_unique<SkipListLevel[]>(level);
+}
+
+SkipListNode::~SkipListNode() {}
+
+SkipList::SkipList() {
+  header_ = std::make_shared<SkipListNode>(kSkipListMaxLevel);
+}
+
+SkipList::SkipList(SkipListType type) : type_(type) {
+  header_ = std::make_shared<SkipListNode>(kSkipListMaxLevel);
+}
+
+SkipList::~SkipList() {}
+
+bool SkipList::Insert(SkipListNodeUakArrUptr uaks) {
+  SkipListNodeArrSptr update =
+      std::make_shared<SkipListNode>(kSkipListMaxLevel);
+  SkipListNodeSptr x = header_;
+  for (int i = level_ - 1; i >= 0; --i) {
+  }
+
+  int level = GetRandomLevel();
+
+  return true;
+}
+
+bool SkipList::Delete(uint64_t uid) { return true; }
+
+bool SkipList::Update(SkipListNodeUakArrUptr uaks) { return true; }
+
+uint64_t SkipList::GetScore(uint64_t uid) { return 0; }
+
+SkipListType SkipList::GetType() { return type_; }
+
+constexpr size_t SkipList::GetKeySizeByType(SkipListType type) {
+  switch (type) {
+    case SkipListType::kSkipListTypeScore: {  // uid + score
+      return 2;
+    } break;
+    case SkipListType::kSkipListTypeScoreP1: {  // uid + score + p1
+      return 3;
+    } break;
+    case SkipListType::kSkipListTypeScoreP1P2: {  // uid + score + p1 + p2
+      return 4;
+    } break;
+    default:
+      break;
+  }
+
+  return 0;  // !!危险 外面使用注意0的情况
+}
+
+int SkipList::GetRandomLevel() {
+  int level = 1;
+  while ((RandI32() & 0xFFFF) < (kSkipListProbability * 0xFFFF)) {
+    ++level;
+  }
+
+  return (level < kSkipListMaxLevel) ? level : kSkipListMaxLevel;
+}
+
+}  // namespace tpn
--- tpn/src/lib/common/rank/skiplist.h
+++ tpn/src/lib/common/rank/skiplist.h
@@ -29,6 +29,9 @@
 
 namespace tpn {
 
+constexpr size_t kSkipListMaxLevel    = 32;    ///< 2 ^ 64 元素
+constexpr double kSkipListProbability = 0.25;  ///< 随机层数概率
+
 /// 跳表节点类型
 enum class SkipListType : uint8_t {
   kSkipListTypeNone      = 0,  ///< 默认
@@ -38,16 +41,95 @@ enum class SkipListType : uint8_t {
 };
 
 class SkipListNode;
-using SkipListNodeSptr = std::shared_ptr<SkipListNode>;
+using SkipListNodeSptr    = std::shared_ptr<SkipListNode>;
+using SkipListNodeArrSptr = std::shared_ptr<SkipListNode[]>;
 
 /// 跳表层结构
-class SkipListLevel {};
+class SkipListLevel {
+ public:
+  SkipListLevel();
+  ~SkipListLevel();
+
+ private:
+  SkipListNodeSptr forward_{nullptr};  ///< 前置跳表节点
+  size_t span_{0};                     ///< 跨越节点
+};
+
+using SkipListLevelArrUptr = std::unique_ptr<SkipListLevel[]>;
+
+using SkipListNodeUakArrUptr = std::unique_ptr<uint64_t[]>;
 
 /// 跳表节点
-class SkipListNode {};
+class SkipListNode {
+ public:
+  /// 构造函数
+  ///  @param[in]   level     当前节点创建层数
+  SkipListNode(int level);
+
+  /// 构造函数
+  ///  @param[in]   level     当前节点创建层数
+  ///  @param[in]   uaks      玩家id与跳表key集合，根据节点类型不同，所需要的参数个数不同
+  SkipListNode(int level, SkipListNodeUakArrUptr uaks);
+
+  ~SkipListNode();
+
+ private:
+  SkipListNodeUakArrUptr uaks_{nullptr};  ///< uid and keys
+  SkipListNodeSptr backward_{nullptr};    ///< 后置跳表节点
+  SkipListLevelArrUptr levels_{nullptr};  ///< 该节点的层数据
+};
 
 /// 跳表
-class SkipList {};
+class SkipList {
+ public:
+  SkipList();
+
+  /// 构造函数
+  ///  @param[in]   type    调表节点类型
+  explicit SkipList(SkipListType type);
+
+  ~SkipList();
+
+  /// 插入数据
+  ///  @param[in]   uaks      玩家id与跳表key集合，根据节点类型不同，所需要的参数个数不同
+  ///  @return 成功返回true
+  bool Insert(SkipListNodeUakArrUptr uaks);
+
+  /// 删除数据
+  ///  @param[in]   uid       玩家id
+  ///  @return 成功返回true
+  bool Delete(uint64_t uid);
+
+  /// 更新数据
+  ///  @param[in]   uaks      玩家id与跳表key集合，根据节点类型不同，所需要的参数个数不同
+  ///  @return 成功返回true
+  bool Update(SkipListNodeUakArrUptr uaks);
+
+  /// 根据玩家id获取分数
+  ///  @param[in]   uid       玩家id
+  ///  @return 返回分数
+  uint64_t GetScore(uint64_t uid);
+
+  /// 获取跳表类型
+  ///  @return 跳表类型
+  SkipListType GetType();
+
+  /// 根据类型获取对应的key的大小
+  ///  @param[in]   type    调表节点类型
+  ///  @return 类型对应的key大小
+  static constexpr size_t GetKeySizeByType(SkipListType type);
+
+  /// 随机层数
+  ///  @return 节点层数
+  static int GetRandomLevel();
+
+ private:
+  SkipListType type_{SkipListType::kSkipListTypeNone};  ///< 跳表节点类型
+  SkipListNodeSptr header_{nullptr};                    ///< 头结点
+  SkipListNodeSptr tail_{nullptr};                      ///< 尾结点
+  size_t length_{0};  ///< 跳表中节点总个数
+  int level_{1};      ///< 跳表中最高层数
+};
 
 }  // namespace tpn
 
