2e0b00ff51a0d00d90d985209f664d667f26ca85
stanleyguo0207 <stanleyguo0207@163.com>
2021-09-11 21:00:59 +0800

    feat: aoi更新
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- tpn/tests/lib/aoi/test_aoi.cpp
+++ tpn/tests/lib/aoi/test_aoi.cpp
@@ -72,8 +72,11 @@ TEST_CASE("data1", "data") {
   AOINode1 *node3 = new AOINode1();
 
   node1->SetExtXYZ(10.f, 10.f, 10.f);
+  node1->SetDescStr("node1");
   node2->SetExtXYZ(20.f, 10.f, 20.f);
+  node2->SetDescStr("node2");
   node3->SetExtXYZ(20.f, 30.f, 10.f);
+  node3->SetDescStr("node3");
 
   aoi_mgr->Insert(node1);
   aoi_mgr->Insert(node2);
